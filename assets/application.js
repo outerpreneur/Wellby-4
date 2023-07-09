209b1318fe4b72da007569ce34fba8b7ba135ae7
Alek Tysklind <91225228+outerpreneur@users.noreply.github.com>
2023-07-10 00:19:51 +0200

    quantity buttons

--- assets/application.js
+++ assets/application.js
@@ -1,1 +1,16 @@
 // Put your applicaiton javascript here
+$(document).ready(function () {
+  $(document).on("click", ".js-quantity-button", function (event) {
+    // alert("It works motherfucker");
+    let $button = $(this),
+      $form = $button.closest("form"),
+      $quantity = $form.find(".js-quantity-field"),
+      quantityValue = parseInt($quantity.val()),
+      max = $quantity.attr("max") ? parseInt($quantity.attr("max")) : null;
+    if ($button.hasClass("plus")) {
+      $quantity.val(quantityValue + 1);
+    } else if ($button.hasClass("minus")) {
+      $quantity.val(quantityValue + 1);
+    }
+  });
+});
