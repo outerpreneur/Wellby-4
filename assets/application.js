c933f1f5724ef2cbc56aef1d13e3ddca7afb4a91
Alek Tysklind <91225228+outerpreneur@users.noreply.github.com>
2023-07-10 22:53:25 +0200

    variant buttons

--- assets/application.js
+++ assets/application.js
@@ -1,4 +1,5 @@
-// Put your applicaiton javascript here
+// Adds or substract on input when clicking plus or minus buttons
+
 $(document).ready(function () {
   $(document).on("click", ".js-quantity-button", function (event) {
     // alert("It works motherfucker");
@@ -7,10 +8,56 @@ $(document).ready(function () {
       $quantity = $form.find(".js-quantity-field"),
       quantityValue = parseInt($quantity.val()),
       max = $quantity.attr("max") ? parseInt($quantity.attr("max")) : null;
-    if ($button.hasClass("plus")) {
-      $quantity.val(quantityValue + 1);
+    if (
+      $button.hasClass("plus") &&
+      (max === null || quantityValue + 1 <= max)
+    ) {
+      $quantity.val(quantityValue + 1).change();
     } else if ($button.hasClass("minus")) {
-      $quantity.val(quantityValue + 1);
+      $quantity.val(quantityValue - 1).change();
+    }
+  });
+
+  // changes the text number when pressing buttons
+
+  $(document).on("change", ".js-quantity-field", function (event) {
+    let $field = $(this),
+      $form = $field.closest("form"),
+      $quantityText = $form.find(".js-quantity-text"),
+      shouldDisableMinus = parseInt(this.value) === 1,
+      shouldDisablePlus = parseInt(this.value) === parseInt($field.attr("max")),
+      $minusButton = $form.find(".js-quantity-button.minus"),
+      $plusButton = $form.find(".js-quantity-button.plus");
+    $quantityText.text(this.value);
+    // enables or disables minus button when 1
+    if (shouldDisableMinus) {
+      $minusButton.prop("disabled", true);
+    } else if ($minusButton.prop("disabled") === true) {
+      $minusButton.prop("disabled", false);
+    }
+
+    if (shouldDisablePlus) {
+      $plusButton.prop("disabled", true);
+    } else if ($minusButton.prop("disabled") === true) {
+      $plusButton.prop("disabled", false);
+    }
+  });
+
+  $(document).on("change", ".js-variant-radio", function (event) {
+    let $radio = $(this),
+      $form = $radio.closest("form"),
+      max = $radio.attr("data-inventory-quantity"),
+      $quantity = $form.find(".js-quantity-field"),
+      $addToCartButton = $form.find("#add-to-cart-button");
+
+    if ($addToCartButton.prop("disabled") === true) {
+      $addToCartButton.prop("disabled", false);
+    }
+
+    $quantity.attr("max", max);
+
+    if (parseInt($quantity.val()) > max) {
+      $quantity.val(max).change();
     }
   });
 });
